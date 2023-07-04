c1ceb7e9436855e75f951586345f31aa11db1b0f
ELEF-TQ <tariq75aitlahklift@gmail.com>
2023-07-04 11:04:03 +0200

    add files

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Home';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React JS CRUD Opertations</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/employee/create' element={<EmpCreate />}></Route>
+
+          <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
+          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+
+}
+
+export default App;
